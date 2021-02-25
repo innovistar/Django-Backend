73ff2314c150b2c162020cb2662b7e93c8df60f8
innovistar <45834254+innovistar@users.noreply.github.com>
2021-02-26 01:49:29 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -65,12 +65,12 @@ AUTH_USER_MODEL = 'users.Account'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_ALLOW_ALL = True
